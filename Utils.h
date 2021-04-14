fc4b5488f188ad310f95d665e2ce6f715d8e57e9
FPS.FirePlanetStudio <57412538+VladimirRepp@users.noreply.github.com>
2021-04-14 22:15:42 +0700

    Create Utils.h

---
+++ Utils.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// Required libraries
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Windows::Forms;
+using namespace System::Data::OleDb;
+
+// Managed Class - as a structure with data
+ref class Field {
+public:
+	int id;
+	String^ name;
+};
+
+// Managed Class
+ref class Utils
+{
+	// Fields
+private: 
+	List<Field^>^ fields; 
+
+	// Methods
+public:
+	Utils();	// default constructor - creates an empty object
+	~Utils();	// destructor - deletes
+	void Download();
+	void Insert();
+	void Insert(Field^);
+	void Update(Field^);
+	void Delete(Field^);
+};
